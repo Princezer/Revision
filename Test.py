eb91915e74b7123d6d844c4666535792a09243d2
Princezer <127698134+Princezer@users.noreply.github.com>
2024-02-13 10:30:42 +1300

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,14 +1,5 @@
-Number = 0
-Happy = ("NO")
-
-while Number < 10:
-    print (Number)
-    Number += 1
-print ("All done!")
+def Function_Word(word):
+    print(f"The best word is {word}. Fight me")
 
 
-while Happy == "NO" or Happy == "N":
-    Happy = input("are you happy?").upper()
-    print (Happy)
-
-print ("Good :]")
+Function_Word("not a tangable thing")
